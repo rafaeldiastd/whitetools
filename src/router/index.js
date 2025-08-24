93ba42b2d20452eb6f858b7ed5058020f1755c48
Rafael Dias <rafaeldiastd@gmail.com>
2025-08-24 04:13:59 -0300

    adjust router

--- src/router/index.js
+++ src/router/index.js
@@ -4,36 +4,36 @@ import BearHiveView from '../views/BearHive/View.vue'
 import ScheduleCreate from '../views/Schedule/Create.vue'
 import ScheduleView from '../views/Schedule/View.vue'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/bearhive/create',
-      name: 'bearhive-create',
-      component: BearHiveCreate,
-    },
-    {
-      path: '/bearhive/view',
-      name: 'bearhive-view',
-      component: BearHiveView,
-    },
-    {
-      path: '/',
-      name: 'schedule-create',
-      component: ScheduleCreate,
-    },
-    {
-      path: '/schedule/:id',
-      name: 'schedule-view',
-      component: ScheduleView,
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      redirect: '/'
-    }
+const routes = [
+  {
+    path: '/bearhive/create',
+    name: 'bearhive-create',
+    component: BearHiveCreate,
+  },
+  {
+    path: '/bearhive/view',
+    name: 'bearhive-view',
+    component: BearHiveView,
+  },
+  {
+    path: '/',
+    name: 'schedule-create',
+    component: ScheduleCreate,
+  },
+  {
+    path: '/schedule/:id',
+    name: 'schedule-view',
+    component: ScheduleView,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/'
+  }
 
-  ],
+],
 
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes
 })
-
-export default router
